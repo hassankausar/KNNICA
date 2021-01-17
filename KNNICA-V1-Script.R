01d6e79fa67a5b9c24f124e886a1099cc3e4f37b
Hassan Kausar <v8039087@live.tees.ac.uk>
2021-01-17 02:38:27 +0000

    Added documentation

--- KNNICA-V1-Script.R
+++ KNNICA-V1-Script.R
@@ -68,8 +68,6 @@ BreastCancer <-
     )
   )
 
-
-
 # Checking the data summary which includes the Mean, Mode and Median.
 summary(BreastCancer)
 
@@ -87,7 +85,6 @@ summary (BreastCancer$Bare_Nuclei[BreastCancer$Class == "2"])
 BreastCancer[-c(139, 145, 158, 249, 275, 294, 321, 411, 617), ] %>% head()
 BreastCancer <-
   BreastCancer[-c(139, 145, 158, 249, 275, 294, 321, 411, 617),]
-
 
 
 # Replacing the missing data by its mode for the Bare Nuclei variable.
@@ -190,7 +187,7 @@ head(BreastCancer_Normalisation)
 set.seed(295)
 
 
-
+# The sample function takes the normalised data that I created above where the class variable was taken off and it distributes into 70/30 proportion.
 BreastCancer3 <-
   sample(
     1:nrow(BreastCancer_Normalisation),
@@ -198,57 +195,82 @@ BreastCancer3 <-
     replace = FALSE
   )
 
-
-
+# The BC_Train is using 70% of data specified above in the BreastCancer3 data we created.
 BC_Train <- BreastCancer_Normalisation[BreastCancer3, ]
+# The BC_Test is using the remaining data which should be 30% specified above in the BreastCancer3 data we created.
 BC_Test <- BreastCancer_Normalisation[-BreastCancer3, ]
 
 
-
+# When I create the training and test data, I excluded the target variable which is the CLASS variable.
+# The labels I am creating below are stored in a separate factor vectors.
+# 11 stands for the 11th variable which was the Class.
+# The training labels are training for the Class variable.
 BC_Train_Labels <- BreastCancer[BreastCancer3, 11]
+# The testing labels are training for the Class variable.
 BC_Test_Labels <- BreastCancer[-BreastCancer3, 11]
 
 
-
+# The KNN function is used to classify the data and it returns a factor vector of predicated labels.
+# I used 21 as it is the close to the square root of our training data.
+# We are training the BC_Train data which was the 70% when I splitted.
+# We are testing the BC_Test data which was the 30% when I splitted.
 BC_Test_Prediction <-
   knn(train = BC_Train, test = BC_Test, cl = BC_Train_Labels, k = 21)
 
-
-
+# Cross table is used to evaluate the model perfomance which is provided by GModels package.
+# The prop.chisq = FALSE removes the chi-square values that are not needed.
+# This gives us our TP. TN. FP. FN. Results
 CrossTable(x = BC_Test_Labels, y = BC_Test_Prediction, prop.chisq = FALSE)
 
 
-# compute classification accuracy for the logistic regression model
-# from sklearn import metrics
-
+# The following line was used to get the overall accuracy for this model.
 Metrics::accuracy(BC_Test_Labels, BC_Test_Prediction)
 
 
-#----------------------------- WORKING WITH KNN-2 -----------------------------
+#------------ IMPROVING MODEL PERFOMANCE USING Z-Score Standardisation----------
 
+# To improve the model we are following the same steps used before but this time using the Z-Score Standardisation.
+
+# Create BreastCancer_Z which gets the data from my original BreastCancer DataSet but excludes the Class variable.
 BreastCancer_Z <- as.data.frame(scale(BreastCancer[-11]))
 
+# The following set.seed() function will help to reuse the same set of random variables.
+# It might be required further in the ICA to evaluate particular task again with same random varibales.
 set.seed(206)
 
-BreastCancer4 <- sample(1:nrow(BreastCancer_Z), size = nrow(BreastCancer_Z) * 0.7, replace = FALSE)
+# The sample function takes the normalised data that I created above where the class variable was taken off and it distributes into 70/30 proportion.
+BreastCancerZ <- sample(1:nrow(BreastCancer_Z), size = nrow(BreastCancer_Z) * 0.7, replace = FALSE)
+
+# The BC_TrainZ is using 70% of data specified above in the BreastCancerZ data we created.
+BC_TrainZ <- BreastCancer_Z[BreastCancerZ, ]
+# The BC_TestZ is using the remaining data which should be 30% specified above in the BreastCancerZ data we created.
+BC_TestZ <- BreastCancer_Z[-BreastCancerZ, ]
+
+# When I create the BreastCancer_Z data, I excluded the target variable which is the CLASS variable.
+# The labels I am creating below are stored in a separate factor vectors.
+# 11 stands for the 11th variable which was the Class from the original dataset.
+# The training labels are training for the Class variable.
+BC_Train_LabelsZ <- BreastCancer[BreastCancerZ, 11]
+BC_Test_LabelsZ <- BreastCancer[-BreastCancerZ, 11]
+
+# The KNN function is used to classify the data and it returns a factor vector of predicated labels.
+# I used 21 as it is the close to the square root of our training data(483).
+# We are training the BC_TrainZ data which was the 70% when I splitted.
+# We are testing the BC_TestZ data which was the 30% when I splitted.
+BC_Test_PredictionZ <-
+  knn(train = BC_TrainZ, test = BC_TestZ, cl = BC_Train_LabelsZ, k = 21)
+
+# Cross table is used to evaluate the model perfomance which is provided by GModels package.
+# The prop.chisq = FALSE removes the chi-square values that are not needed.
+# This gives us our TP. TN. FP. FN. Results
+CrossTable(x = BC_Test_LabelsZ, y = BC_Test_PredictionZ, prop.chisq = FALSE)
+
+# Dummy Plot
+plot(x = BC_Test_LabelsZ, y = BC_Test_PredictionZ)
 
 
-BC_Train2 <- BreastCancer_Z[BreastCancer4, ]
-BC_Test2 <- BreastCancer_Z[-BreastCancer4, ]
 
-
-BC_Train_Labels2 <- BreastCancer[BreastCancer4, 11]
-BC_Test_Labels2 <- BreastCancer[-BreastCancer4, 11]
-
-
-BC_Test_Prediction2 <-
-  knn(train = BC_Train2, test = BC_Test2, cl = BC_Train_Labels2, k = 23)
-
-CrossTable(x = BC_Test_Labels2, y = BC_Test_Prediction2, prop.chisq = FALSE)
-plot(x = BC_Test_Labels2, y = BC_Test_Prediction2)
-
-
-
-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Display the accuracies for both version that I have worked with.
 Metrics::accuracy(BC_Test_Labels, BC_Test_Prediction)
-Metrics::accuracy(BC_Test_Labels2, BC_Test_Prediction2)
+Metrics::accuracy(BC_Test_LabelsZ, BC_Test_PredictionZ)
